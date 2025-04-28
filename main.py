34c218a064f317905301398ccaddd112df782334
Vitor Borin <164498280+Vitor-Borin@users.noreply.github.com>
2025-04-28 18:47:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-import os
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from rag_system import ask_question
 from speech import generate_speech
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 class QuestionRequest(BaseModel):
     question: str
@@ -37,8 +27,3 @@ async def speech(request: SpeechRequest):
 @app.get("/")
 def read_root():
     return {"message": "API da IA rodando! 🚀"}
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
